c22f3c55c153f7b790164e95dabaaa114c2a5d05
Fred Sells <fsells@provinet.com>
2019-08-08 15:21:48 -0400

    remove clear cache and add db

--- dakota/settings.py
+++ dakota/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'practice',
-    'clear_cache',
-    'favicon',
-    'widget_tweaks',
+ #   'clear_cache',
+ #   'favicon',
+ #   'widget_tweaks',
 ]
 
 MIDDLEWARE = [
